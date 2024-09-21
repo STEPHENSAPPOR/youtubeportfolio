ac914f97ae48171b110026e2f150167d48ebc632
stephensappor <stephensappor5@gmail.com>
2024-09-21 10:54:15 +0000

    design

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,7 +1,14 @@
-import React from 'react'
-import Title from '../layouts/Title'
-import { projectOne, projectTwo, projectThree, tube, imperial, fleetz } from "../../assets/index";
-import ProjectsCard from './ProjectsCard';
+import React from "react";
+import Title from "../layouts/Title";
+import {
+  projectOne,
+  projectTwo,
+  projectThree,
+  tube,
+  imperial,
+  fleetz,
+} from "../../assets/index";
+import ProjectsCard from "./ProjectsCard";
 
 const Projects = () => {
   return (
@@ -11,7 +18,7 @@ const Projects = () => {
     >
       <div className="flex justify-center items-center text-center">
         <Title
-          title="VISIT MY PORTFOLIO AND KEEP YOUR FEEDBACK"
+          title="VISIT MY PORTFOLIO AND GIVE YOUR FEEDBACK"
           des="My Projects"
         />
       </div>
@@ -22,18 +29,37 @@ const Projects = () => {
            featuring a modern design with seamless user interaction.
            The site includes detailed event information to enhance user experience.!"
           src={projectOne}
+          githubLink="https://github.com/M-S-D-E/M-S-D-E-Event-Frontend"
         />
         <ProjectsCard
-          title="RECIPE APP"
-          des=" I built a sleek and modern Italian restaurant website using HTML, CSS, and JavaScript.
-           The site features a responsive design with rich visuals to showcase the restaurant's menu and ambiance.!"
+          title="DevLink"
+          des="I developed a dynamic and user-friendly platform using the React framework for Devlink,
+ where developers can create profiles, bid on projects, and get paid. 
+ The site enables clients to post projects.!"
           src={projectTwo}
+          githubLink="https://github.com/DevLink-hub/DevLink-Frontend"
         />
         <ProjectsCard
           title="Ghana Hazard Project"
           des=" I developed a Ghana Hazard Reporter platform using React, TypeScript, 
           and Tailwind CSS, focusing on a responsive design and user-friendly interface for reporting and viewing hazards.!"
           src={projectThree}
+          githubLink="https://github.com/STEPHENSAPPOR/ghana-hazard-reporter"
+        />
+         <ProjectsCard
+          title="RoxyApps"
+          des="I built a digital solutions platform using React for a software company.
+           The platform streamlines existing business processes across web, mobile, and desktop platforms,
+           offering comprehensive solutions.!"
+          src={fleetz}
+          githubLink="https://github.com/STEPHENSAPPOR/RoxyApps"
+        />
+         <ProjectsCard
+          title="Imperial"
+          des=" I built a sleek and modern Italian restaurant website using HTML, CSS, and JavaScript.
+           The site features a responsive design with rich visuals to showcase the restaurant's menu and ambiance.!"
+          src={imperial}
+          githubLink="https://github.com/STEPHENSAPPOR/Imperial"
         />
         <ProjectsCard
           title="YouTube Clone"
@@ -41,22 +67,13 @@ const Projects = () => {
           HTML, CSS, and JavaScript, featuring a responsive design, video thumbnails, and basic interactive elements
            like search and video playback.!"
           src={tube}
+          githubLink="https://github.com/STEPHENSAPPOR/youtube"
         />
-        <ProjectsCard
-          title="Imperial"
-          des=" I built a sleek and modern Italian restaurant website using HTML, CSS, and JavaScript.
-           The site features a responsive design with rich visuals to showcase the restaurant's menu and ambiance.!"
-          src={imperial}
-        />
-        <ProjectsCard
-          title="Fleetz"
-          des=" I built a sleek and modern Car Collection website using HTML, CSS, and JavaScript.
-           The site features a  design with rich visuals to showcase Luxurious Cars.!"
-          src={fleetz}
-        />
+       
+       
       </div>
     </section>
   );
-}
+};
 
-export default Projects
+export default Projects;
